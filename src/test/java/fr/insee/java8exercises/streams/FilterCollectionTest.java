92ec33d19483fa54d1ae30088eea00c81d29a89e
Romain Warnan <romain.warnan@insee.fr>
2018-02-28 09:27:38 +0100

    Renommage d'un test

---
+++ src/test/java/fr/insee/java8exercises/streams/FilterCollectionTest.java
@@ -0,0 +1,20 @@
+package fr.insee.java8exercises.streams;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static fr.insee.java8exercises.streams.FilterCollection.*;
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FilterCollectionTest {
+
+    @Test
+    public void transformKeepStringsShorterThant4Characters() {
+        List<String> collection = asList("My", "name", "is", "John", "Doe");
+        List<String> expected = asList("My", "is", "Doe");
+        assertThat(stringsShorterThant4Characters(collection)).hasSameElementsAs(expected);
+    }
+
+}
