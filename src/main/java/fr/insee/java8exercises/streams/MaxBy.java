19b5312674e6feb536e59d95463915d51266fa37
Romain Warnan <romain.warnan@insee.fr>
2018-03-01 08:25:34 +0100

    Correction maxBy

--- src/main/java/fr/insee/java8exercises/streams/MaxBy.java
+++ src/main/java/fr/insee/java8exercises/streams/MaxBy.java
@@ -1,10 +1,9 @@
 package fr.insee.java8exercises.streams;
 
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Optional;
+import static java.util.stream.Collectors.*;
 
 public class MaxBy {
 
@@ -12,20 +11,12 @@ public class MaxBy {
     }
     
 	public static String mostFrequentNationality(List<Person> people) {
-		Map<String, Integer> map = new HashMap<>();
-		for (Person person : people) {
-			String nationality = person.getNationality();
-			Integer count = Optional.ofNullable(map.get(nationality)).orElse(0);
-			map.put(nationality, count + 1);
-		}
-		int max = 0;
-		String mostFrequentNationality = null;
-		for (Entry<String, Integer> entry : map.entrySet()) {
-			if (entry.getValue() > max) {
-				max = entry.getValue();
-				mostFrequentNationality = entry.getKey();
-			}
-		}
-		return mostFrequentNationality;
+		return people.stream()
+			.collect(groupingBy(Person::getNationality, counting()))
+			.entrySet()
+			.stream()
+			.max(Comparator.comparingLong(Entry::getValue))
+			.map(Entry::getKey)
+			.get();
 	}
 }
